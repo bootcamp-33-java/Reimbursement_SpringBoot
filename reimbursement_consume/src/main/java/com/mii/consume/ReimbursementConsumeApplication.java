5aaf7e1abd6470e990081ec5d17548b6ab71720d
Yuny Astutik <yunyastutik@gmail.com>
2020-02-29 06:52:57 -0800

    Add files via upload

---
+++ reimbursement_consume/src/main/java/com/mii/consume/ReimbursementConsumeApplication.java
@@ -0,0 +1,13 @@
+package com.mii.consume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReimbursementConsumeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReimbursementConsumeApplication.class, args);
+	}
+
+}
---
+++ reimbursement_consume/src/test/java/com/mii/consume/ReimbursementConsumeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mii.consume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReimbursementConsumeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
