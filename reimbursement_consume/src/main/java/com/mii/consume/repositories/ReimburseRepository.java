b5a87f30d93dda96724f02dbbcb28ae3ce57debb
yuyun <yunyastutik@gmail.com>
2020-03-01 16:12:11 +0700

    add reimbursement

--- reimbursement_consume/src/main/java/com/mii/consume/repositories/ReimburseRepository.java
+++ reimbursement_consume/src/main/java/com/mii/consume/repositories/ReimburseRepository.java
@@ -5,8 +5,11 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ReimburseRepository extends PagingAndSortingRepository<Reimburse, String> {
     List<Reimburse> findAll();
+
+    Optional<Reimburse> findById(String id);
 }
